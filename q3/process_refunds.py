5e62db77b3ed734596e04fcba47115f0425bca1a
Josh Yeung <joshyeung126@gmail.com>
2026-07-07 22:18:16 -0400

    q3

--- q3/process_refunds.py
+++ q3/process_refunds.py
@@ -6,14 +6,47 @@ client = StripeClient("sk_test_123", base_addresses={"api": "http://localhost:12
 
 def load_cancellations(filepath):
     with open(filepath, 'r') as f:
-        return json.load(f)
+        object = json.load(f)
+        for o in object:
+            o["ikey"] = uuid.uuid4()
+        return object
+
+
 
 def run_refund_job():
     orders_to_refund = load_cancellations("cancellations.json")
     print(f"Starting refund job for {len(orders_to_refund)} orders...")
     
     # TODO: Implement the refund logic here
-            
+    try:
+        for orders in orders_to_refund:
+            order_id = orders["order_id"]
+            query = f"metadata['order_id']:'{order_id}'"
+            response = client.v1.charges.search({"query": query})
+            response = response.data[0]
+            if not response:
+                print("Warning no charge")
+                continue
+            if response.status == "requires_capture":
+                client.v1.payment_intents.cancel(response["payment_intent"])
+                with open("successful_refunds.txt", "a") as f:
+                    f.write(f"Order {response.id} refunded via payment\n")
+                
+            else:
+                refund = client.v1.refunds.create(
+                    params={
+                        "charge": response.id,
+                        "reason": "requested_by_customer"
+                    },
+                    options={"idempotency_key": str(orders["ikey"])}
+                )
+                print(refund)
+                with open("successful_refunds.txt", "a") as f:
+                    f.write(f"Order {response.id} refunded via Refund ID: {refund.id}\n")
+                
+    except Exception as e:
+        print(f"BAD! {e}")
+
     print("Refund job completed.")
 
 if __name__ == "__main__":
